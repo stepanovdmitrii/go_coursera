7709b9969e20607d2bae6b826e336983a82268e1
Dmitrii <dima-stepan@yandex.ru>
2021-04-25 21:45:45 +0300

    hw5 codegen - pass tests

--- hw5_codegen/api_handlers.go
+++ hw5_codegen/api_handlers.go
@@ -1,8 +1,196 @@
 package main
 
+import "fmt"
+import "strconv"
+import "strings"
+import "encoding/json"
+import "net/http"
+
+func (in *ProfileParams) ApiGenSet(r *http.Request) error {
+
+	// Login
+
+
+	LoginRaw := r.URL.Query().Get("login")
+	if r.Method == "POST" {
+		LoginRaw = r.FormValue("login")
+	}
+
+	if true && LoginRaw == "" {
+		return fmt.Errorf("login must me not empty")
+	}
+
+	if false && LoginRaw == "" {
+		LoginRaw = ""
+	}
+
+	if false {
+		isValid := false
+		for _, enumValue := range strings.Split("", "|") {
+			isValid = isValid || LoginRaw == enumValue
+		}
+		if !isValid {
+			return fmt.Errorf("login must be one of [" + strings.Join(strings.Split("", "|"), ", ") + "]")
+		}
+	}
+
+	if false && len(LoginRaw) < 0 {
+		return fmt.Errorf("login len must be >= 0")
+	}
+	in.Login = LoginRaw
+    return nil
+}
+
+func (in *CreateParams) ApiGenSet(r *http.Request) error {
+
+	// Login
+
+
+	LoginRaw := r.URL.Query().Get("login")
+	if r.Method == "POST" {
+		LoginRaw = r.FormValue("login")
+	}
+
+	if true && LoginRaw == "" {
+		return fmt.Errorf("login must me not empty")
+	}
+
+	if false && LoginRaw == "" {
+		LoginRaw = ""
+	}
+
+	if false {
+		isValid := false
+		for _, enumValue := range strings.Split("", "|") {
+			isValid = isValid || LoginRaw == enumValue
+		}
+		if !isValid {
+			return fmt.Errorf("login must be one of [" + strings.Join(strings.Split("", "|"), ", ") + "]")
+		}
+	}
+
+	if true && len(LoginRaw) < 10 {
+		return fmt.Errorf("login len must be >= 10")
+	}
+	in.Login = LoginRaw
+
+	// Name
+
+
+	NameRaw := r.URL.Query().Get("full_name")
+	if r.Method == "POST" {
+		NameRaw = r.FormValue("full_name")
+	}
+
+	if false && NameRaw == "" {
+		return fmt.Errorf("full_name must me not empty")
+	}
+
+	if false && NameRaw == "" {
+		NameRaw = ""
+	}
+
+	if false {
+		isValid := false
+		for _, enumValue := range strings.Split("", "|") {
+			isValid = isValid || NameRaw == enumValue
+		}
+		if !isValid {
+			return fmt.Errorf("full_name must be one of [" + strings.Join(strings.Split("", "|"), ", ") + "]")
+		}
+	}
+
+	if false && len(NameRaw) < 0 {
+		return fmt.Errorf("full_name len must be >= 0")
+	}
+	in.Name = NameRaw
+
+	// Status
+
+
+	StatusRaw := r.URL.Query().Get("status")
+	if r.Method == "POST" {
+		StatusRaw = r.FormValue("status")
+	}
+
+	if false && StatusRaw == "" {
+		return fmt.Errorf("status must me not empty")
+	}
+
+	if true && StatusRaw == "" {
+		StatusRaw = "user"
+	}
+
+	if true {
+		isValid := false
+		for _, enumValue := range strings.Split("user|moderator|admin", "|") {
+			isValid = isValid || StatusRaw == enumValue
+		}
+		if !isValid {
+			return fmt.Errorf("status must be one of [" + strings.Join(strings.Split("user|moderator|admin", "|"), ", ") + "]")
+		}
+	}
+
+	if false && len(StatusRaw) < 0 {
+		return fmt.Errorf("status len must be >= 0")
+	}
+	in.Status = StatusRaw
+
+	// Age
+	AgeRaw := r.URL.Query().Get("age")
+	if r.Method == "POST" {
+		AgeRaw = r.FormValue("age")
+	}
+
+	var AgeValue int
+
+	if false && AgeRaw == "" {
+		return fmt.Errorf("age must me not empty")
+	}
+
+	if AgeRaw != "" {
+		AgeConvValue, AgeErr := strconv.Atoi(AgeRaw)
+		if AgeErr !=nil {
+			return fmt.Errorf("age must be int")
+		}
+		AgeValue = AgeConvValue
+	}
+
+	if false && AgeRaw == "" {
+		if "" != "" {
+			AgeValue, _ = strconv.Atoi("")
+		}
+		
+	}
+
+	if false {
+		isValid := false
+
+		for _, enumValue := range strings.Split("", "|") {
+			enumIntValue, _ := strconv.Atoi(enumValue)
+			isValid = isValid || AgeValue == enumIntValue
+		}
+
+		if !isValid {
+			return fmt.Errorf("age must be one of [" + strings.Join(strings.Split("", "|"), ", ") + "]")
+		}
+	}
+
+	if true && AgeValue < 0 {
+		return fmt.Errorf("age must be >= 0")
+	}
+
+	if true && AgeValue > 128 {
+		return fmt.Errorf("age must be <= 128")
+	}
+
+	in.Age = AgeValue
+    return nil
+}
+
 
 func (api *MyApi) wrapperApiGenProfile(w http.ResponseWriter, r *http.Request){
-	if r.Method != "" {
+	if "" != "" && r.Method != "" {
 		w.WriteHeader(http.StatusNotAcceptable)
 		w.Write([]byte("{\"error\" : \"bad method\"}"))
 		return
@@ -15,37 +203,38 @@ func (api *MyApi) wrapperApiGenProfile(w http.ResponseWriter, r *http.Request){
 	}
 
 	var p ProfileParams
-	p.fillApiGen(r)
-	validateErr := p.validateApiGen()
-	if validateErr != nil {
+	setErr := p.ApiGenSet(r)
+	if setErr != nil {
 		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte("{\"error\" : \"" + validateErr.Error() +"\"}"))
+		w.Write([]byte("{\"error\" : \"" + strings.ToLower(setErr.Error()) +"\"}"))
 		return
 	}
 	
-	res, err := h.Profile(r.Context(), p)
+	res, err := api.Profile(r.Context(), p)
 
 	if err != nil {
-		switch v := err.(type){
+		switch err.(type){
 		case ApiError:
 			apiErr := err.(ApiError)
 			w.WriteHeader(apiErr.HTTPStatus)
-			w.Write([]byte("{\"error\" : \"" + apiErr.Err.Error() +"\"}"))
+			w.Write([]byte("{\"error\" : \"" + strings.ToLower(apiErr.Err.Error()) +"\"}"))
 		default:
 			w.WriteHeader(http.StatusInternalServerError)
-			w.Write([]byte("{\"error\" : \"" + err.Error() +"\"}"))
+			w.Write([]byte("{\"error\" : \"" + strings.ToLower(err.Error()) +"\"}"))
 		}
 		return
 	}
 
-	data, _ := json.Marshal(res)
+	mapResponse := map[string]interface{}{"error": "", "response": res}
+
+	data, _ := json.Marshal(mapResponse)
 	w.WriteHeader(http.StatusOK)
 	w.Write(data)
 	return
 }
 
 func (api *MyApi) wrapperApiGenCreate(w http.ResponseWriter, r *http.Request){
-	if r.Method != "POST" {
+	if "POST" != "" && r.Method != "POST" {
 		w.WriteHeader(http.StatusNotAcceptable)
 		w.Write([]byte("{\"error\" : \"bad method\"}"))
 		return
@@ -58,37 +247,185 @@ func (api *MyApi) wrapperApiGenCreate(w http.ResponseWriter, r *http.Request){
 	}
 
 	var p CreateParams
-	p.fillApiGen(r)
-	validateErr := p.validateApiGen()
-	if validateErr != nil {
+	setErr := p.ApiGenSet(r)
+	if setErr != nil {
 		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte("{\"error\" : \"" + validateErr.Error() +"\"}"))
+		w.Write([]byte("{\"error\" : \"" + strings.ToLower(setErr.Error()) +"\"}"))
 		return
 	}
 	
-	res, err := h.Create(r.Context(), p)
+	res, err := api.Create(r.Context(), p)
 
 	if err != nil {
-		switch v := err.(type){
+		switch err.(type){
 		case ApiError:
 			apiErr := err.(ApiError)
 			w.WriteHeader(apiErr.HTTPStatus)
-			w.Write([]byte("{\"error\" : \"" + apiErr.Err.Error() +"\"}"))
+			w.Write([]byte("{\"error\" : \"" + strings.ToLower(apiErr.Err.Error()) +"\"}"))
 		default:
 			w.WriteHeader(http.StatusInternalServerError)
-			w.Write([]byte("{\"error\" : \"" + err.Error() +"\"}"))
+			w.Write([]byte("{\"error\" : \"" + strings.ToLower(err.Error()) +"\"}"))
 		}
 		return
 	}
 
-	data, _ := json.Marshal(res)
+	mapResponse := map[string]interface{}{"error": "", "response": res}
+
+	data, _ := json.Marshal(mapResponse)
 	w.WriteHeader(http.StatusOK)
 	w.Write(data)
 	return
 }
+func (in *OtherCreateParams) ApiGenSet(r *http.Request) error {
+
+	// Username
+
+
+	UsernameRaw := r.URL.Query().Get("username")
+	if r.Method == "POST" {
+		UsernameRaw = r.FormValue("username")
+	}
+
+	if true && UsernameRaw == "" {
+		return fmt.Errorf("username must me not empty")
+	}
+
+	if false && UsernameRaw == "" {
+		UsernameRaw = ""
+	}
+
+	if false {
+		isValid := false
+		for _, enumValue := range strings.Split("", "|") {
+			isValid = isValid || UsernameRaw == enumValue
+		}
+		if !isValid {
+			return fmt.Errorf("username must be one of [" + strings.Join(strings.Split("", "|"), ", ") + "]")
+		}
+	}
+
+	if true && len(UsernameRaw) < 3 {
+		return fmt.Errorf("username len must be >= 3")
+	}
+	in.Username = UsernameRaw
+
+	// Name
+
+
+	NameRaw := r.URL.Query().Get("account_name")
+	if r.Method == "POST" {
+		NameRaw = r.FormValue("account_name")
+	}
+
+	if false && NameRaw == "" {
+		return fmt.Errorf("account_name must me not empty")
+	}
+
+	if false && NameRaw == "" {
+		NameRaw = ""
+	}
+
+	if false {
+		isValid := false
+		for _, enumValue := range strings.Split("", "|") {
+			isValid = isValid || NameRaw == enumValue
+		}
+		if !isValid {
+			return fmt.Errorf("account_name must be one of [" + strings.Join(strings.Split("", "|"), ", ") + "]")
+		}
+	}
+
+	if false && len(NameRaw) < 0 {
+		return fmt.Errorf("account_name len must be >= 0")
+	}
+	in.Name = NameRaw
+
+	// Class
+
+
+	ClassRaw := r.URL.Query().Get("class")
+	if r.Method == "POST" {
+		ClassRaw = r.FormValue("class")
+	}
+
+	if false && ClassRaw == "" {
+		return fmt.Errorf("class must me not empty")
+	}
+
+	if true && ClassRaw == "" {
+		ClassRaw = "warrior"
+	}
+
+	if true {
+		isValid := false
+		for _, enumValue := range strings.Split("warrior|sorcerer|rouge", "|") {
+			isValid = isValid || ClassRaw == enumValue
+		}
+		if !isValid {
+			return fmt.Errorf("class must be one of [" + strings.Join(strings.Split("warrior|sorcerer|rouge", "|"), ", ") + "]")
+		}
+	}
+
+	if false && len(ClassRaw) < 0 {
+		return fmt.Errorf("class len must be >= 0")
+	}
+	in.Class = ClassRaw
+
+	// Level
+	LevelRaw := r.URL.Query().Get("level")
+	if r.Method == "POST" {
+		LevelRaw = r.FormValue("level")
+	}
+
+	var LevelValue int
+
+	if false && LevelRaw == "" {
+		return fmt.Errorf("level must me not empty")
+	}
+
+	if LevelRaw != "" {
+		LevelConvValue, LevelErr := strconv.Atoi(LevelRaw)
+		if LevelErr !=nil {
+			return fmt.Errorf("level must be int")
+		}
+		LevelValue = LevelConvValue
+	}
+
+	if false && LevelRaw == "" {
+		if "" != "" {
+			LevelValue, _ = strconv.Atoi("")
+		}
+		
+	}
+
+	if false {
+		isValid := false
+
+		for _, enumValue := range strings.Split("", "|") {
+			enumIntValue, _ := strconv.Atoi(enumValue)
+			isValid = isValid || LevelValue == enumIntValue
+		}
+
+		if !isValid {
+			return fmt.Errorf("level must be one of [" + strings.Join(strings.Split("", "|"), ", ") + "]")
+		}
+	}
+
+	if true && LevelValue < 1 {
+		return fmt.Errorf("level must be >= 1")
+	}
+
+	if true && LevelValue > 50 {
+		return fmt.Errorf("level must be <= 50")
+	}
+
+	in.Level = LevelValue
+    return nil
+}
+
 
 func (api *OtherApi) wrapperApiGenCreate(w http.ResponseWriter, r *http.Request){
-	if r.Method != "POST" {
+	if "POST" != "" && r.Method != "POST" {
 		w.WriteHeader(http.StatusNotAcceptable)
 		w.Write([]byte("{\"error\" : \"bad method\"}"))
 		return
@@ -101,31 +438,52 @@ func (api *OtherApi) wrapperApiGenCreate(w http.ResponseWriter, r *http.Request)
 	}
 
 	var p OtherCreateParams
-	p.fillApiGen(r)
-	validateErr := p.validateApiGen()
-	if validateErr != nil {
+	setErr := p.ApiGenSet(r)
+	if setErr != nil {
 		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte("{\"error\" : \"" + validateErr.Error() +"\"}"))
+		w.Write([]byte("{\"error\" : \"" + strings.ToLower(setErr.Error()) +"\"}"))
 		return
 	}
 	
-	res, err := h.Create(r.Context(), p)
+	res, err := api.Create(r.Context(), p)
 
 	if err != nil {
-		switch v := err.(type){
+		switch err.(type){
 		case ApiError:
 			apiErr := err.(ApiError)
 			w.WriteHeader(apiErr.HTTPStatus)
-			w.Write([]byte("{\"error\" : \"" + apiErr.Err.Error() +"\"}"))
+			w.Write([]byte("{\"error\" : \"" + strings.ToLower(apiErr.Err.Error()) +"\"}"))
 		default:
 			w.WriteHeader(http.StatusInternalServerError)
-			w.Write([]byte("{\"error\" : \"" + err.Error() +"\"}"))
+			w.Write([]byte("{\"error\" : \"" + strings.ToLower(err.Error()) +"\"}"))
 		}
 		return
 	}
 
-	data, _ := json.Marshal(res)
+	mapResponse := map[string]interface{}{"error": "", "response": res}
+
+	data, _ := json.Marshal(mapResponse)
 	w.WriteHeader(http.StatusOK)
 	w.Write(data)
 	return
+}
+func (api *MyApi) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+    switch r.URL.Path {
+    case "/user/profile":
+        api.wrapperApiGenProfile(w,r)
+    case "/user/create":
+        api.wrapperApiGenCreate(w,r)
+    default:
+        w.WriteHeader(http.StatusNotFound)
+        w.Write([]byte("{\"error\": \"unknown method\"}"))
+    }
+}
+func (api *OtherApi) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+    switch r.URL.Path {
+    case "/user/create":
+        api.wrapperApiGenCreate(w,r)
+    default:
+        w.WriteHeader(http.StatusNotFound)
+        w.Write([]byte("{\"error\": \"unknown method\"}"))
+    }
 }
--- hw5_codegen/handlers_gen/codegen.go
+++ hw5_codegen/handlers_gen/codegen.go
@@ -7,6 +7,8 @@ import (
 	"go/parser"
 	"go/token"
 	"os"
+	"reflect"
+	"strconv"
 	"strings"
 	"text/template"
 )
@@ -25,9 +27,28 @@ type MethodWrapperParams struct {
 	ApiGenParamsType string
 }
 
+type MethodSetParams struct {
+	FieldName    string
+	ParamName    string
+	IsRequired   bool
+	HasEnum      bool
+	EnumString   string
+	HasDefault   bool
+	DefaultValue string
+	HasMin       bool
+	Min          int
+	HasMax       bool
+	Max          int
+}
+
+type ServeHttpParams struct {
+	Url     string
+	Handler string
+}
+
 var wrapperTemplate = template.Must(template.New("wrapperTemplate").Parse(`
 func (api *{{.ApiType}}) wrapperApiGen{{.ApiGenMethodName}}(w http.ResponseWriter, r *http.Request){
-	if r.Method != "{{.ApiGenHttpMethod}}" {
+	if "{{.ApiGenHttpMethod}}" != "" && r.Method != "{{.ApiGenHttpMethod}}" {
 		w.WriteHeader(http.StatusNotAcceptable)
 		w.Write([]byte("{\"error\" : \"bad method\"}"))
 		return
@@ -40,37 +61,124 @@ func (api *{{.ApiType}}) wrapperApiGen{{.ApiGenMethodName}}(w http.ResponseWrite
 	}
 
 	var p {{.ApiGenParamsType}}
-	p.fillApiGen(r)
-	validateErr := p.validateApiGen()
-	if validateErr != nil {
+	setErr := p.ApiGenSet(r)
+	if setErr != nil {
 		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte("{\"error\" : \"" + validateErr.Error() +"\"}"))
+		w.Write([]byte("{\"error\" : \"" + strings.ToLower(setErr.Error()) +"\"}"))
 		return
 	}
 	
-	res, err := h.{{.ApiGenMethodName}}(r.Context(), p)
+	res, err := api.{{.ApiGenMethodName}}(r.Context(), p)
 
 	if err != nil {
-		switch v := err.(type){
+		switch err.(type){
 		case ApiError:
 			apiErr := err.(ApiError)
 			w.WriteHeader(apiErr.HTTPStatus)
-			w.Write([]byte("{\"error\" : \"" + apiErr.Err.Error() +"\"}"))
+			w.Write([]byte("{\"error\" : \"" + strings.ToLower(apiErr.Err.Error()) +"\"}"))
 		default:
 			w.WriteHeader(http.StatusInternalServerError)
-			w.Write([]byte("{\"error\" : \"" + err.Error() +"\"}"))
+			w.Write([]byte("{\"error\" : \"" + strings.ToLower(err.Error()) +"\"}"))
 		}
 		return
 	}
 
-	data, _ := json.Marshal(res)
+	mapResponse := map[string]interface{}{"error": "", "response": res}
+
+	data, _ := json.Marshal(mapResponse)
 	w.WriteHeader(http.StatusOK)
 	w.Write(data)
 	return
 }
 `))
 
+var strTpl = template.Must(template.New("strTpl").Parse(`
+	// {{.FieldName}}
+
+
+	{{.FieldName}}Raw := r.URL.Query().Get("{{.ParamName}}")
+	if r.Method == "POST" {
+		{{.FieldName}}Raw = r.FormValue("{{.ParamName}}")
+	}
+
+	if {{.IsRequired}} && {{.FieldName}}Raw == "" {
+		return fmt.Errorf("{{.ParamName}} must me not empty")
+	}
+
+	if {{.HasDefault}} && {{.FieldName}}Raw == "" {
+		{{.FieldName}}Raw = "{{.DefaultValue}}"
+	}
+
+	if {{.HasEnum}} {
+		isValid := false
+		for _, enumValue := range strings.Split("{{.EnumString}}", "|") {
+			isValid = isValid || {{.FieldName}}Raw == enumValue
+		}
+		if !isValid {
+			return fmt.Errorf("{{.ParamName}} must be one of [" + strings.Join(strings.Split("{{.EnumString}}", "|"), ", ") + "]")
+		}
+	}
+
+	if {{.HasMin}} && len({{.FieldName}}Raw) < {{.Min}} {
+		return fmt.Errorf("{{.ParamName}} len must be >= {{.Min}}")
+	}
+	in.{{.FieldName}} = {{.FieldName}}Raw
+`))
+
+var intTpl = template.Must(template.New("intTpl").Parse(`
+	// {{.FieldName}}
+	{{.FieldName}}Raw := r.URL.Query().Get("{{.ParamName}}")
+	if r.Method == "POST" {
+		{{.FieldName}}Raw = r.FormValue("{{.ParamName}}")
+	}
+
+	var {{.FieldName}}Value int
+
+	if {{.IsRequired}} && {{.FieldName}}Raw == "" {
+		return fmt.Errorf("{{.ParamName}} must me not empty")
+	}
+
+	if {{.FieldName}}Raw != "" {
+		{{.FieldName}}ConvValue, {{.FieldName}}Err := strconv.Atoi({{.FieldName}}Raw)
+		if {{.FieldName}}Err !=nil {
+			return fmt.Errorf("{{.ParamName}} must be int")
+		}
+		{{.FieldName}}Value = {{.FieldName}}ConvValue
+	}
+
+	if {{.HasDefault}} && {{.FieldName}}Raw == "" {
+		if "{{.DefaultValue}}" != "" {
+			{{.FieldName}}Value, _ = strconv.Atoi("{{.DefaultValue}}")
+		}
+		
+	}
+
+	if {{.HasEnum}} {
+		isValid := false
+
+		for _, enumValue := range strings.Split("{{.EnumString}}", "|") {
+			enumIntValue, _ := strconv.Atoi(enumValue)
+			isValid = isValid || {{.FieldName}}Value == enumIntValue
+		}
+
+		if !isValid {
+			return fmt.Errorf("{{.ParamName}} must be one of [" + strings.Join(strings.Split("{{.EnumString}}", "|"), ", ") + "]")
+		}
+	}
+
+	if {{.HasMin}} && {{.FieldName}}Value < {{.Min}} {
+		return fmt.Errorf("{{.ParamName}} must be >= {{.Min}}")
+	}
+
+	if {{.HasMax}} && {{.FieldName}}Value > {{.Max}} {
+		return fmt.Errorf("{{.ParamName}} must be <= {{.Max}}")
+	}
+
+	in.{{.FieldName}} = {{.FieldName}}Value
+`))
+
 func main() {
+	aggr := map[string][]ServeHttpParams{}
 	fset := token.NewFileSet()
 
 	file, err := parser.ParseFile(fset, os.Args[1], nil, parser.ParseComments)
@@ -85,18 +193,141 @@ func main() {
 
 	fmt.Fprintln(out, `package `+file.Name.Name)
 	fmt.Fprintln(out) // empty line
+	fmt.Fprintln(out, `import "fmt"`)
+	fmt.Fprintln(out, `import "strconv"`)
+	fmt.Fprintln(out, `import "strings"`)
+	fmt.Fprintln(out, `import "encoding/json"`)
+	fmt.Fprintln(out, `import "net/http"`)
+	fmt.Fprintln(out) // empty line
 
 	for _, dec := range file.Decls {
 		fun, ok := dec.(*ast.FuncDecl)
+		if ok {
+			generateFunc(fun, out, aggr)
+		}
+
+		g, ok := dec.(*ast.GenDecl)
 		if !ok {
 			continue
-		} else {
-			generateFunc(fun, out)
 		}
+
+		for _, spec := range g.Specs {
+			currType, ok := spec.(*ast.TypeSpec)
+			if !ok {
+				continue
+			}
+
+			currStruct, ok := currType.Type.(*ast.StructType)
+			if !ok {
+				continue
+			}
+
+			if currStruct.Fields == nil {
+				continue
+			}
+
+			if len(currStruct.Fields.List) == 0 || currStruct.Fields.List[0].Tag == nil {
+				continue
+			}
+
+			needCodegen := strings.Contains(currStruct.Fields.List[0].Tag.Value, "apivalidator:")
+
+			if needCodegen {
+				generateStruct(currType, currStruct, out)
+			}
+		}
+	}
+	generateServeHttp(aggr, out)
+}
+
+func generateServeHttp(aggr map[string][]ServeHttpParams, out *os.File) {
+	for k, v := range aggr {
+		fmt.Fprintln(out, "func (api *"+k+") ServeHTTP(w http.ResponseWriter, r *http.Request) {")
+		fmt.Fprintln(out, "    switch r.URL.Path {")
+		for _, m := range v {
+			fmt.Fprintln(out, "    case \""+m.Url+"\":")
+			fmt.Fprintln(out, "        api."+m.Handler+"(w,r)")
+		}
+		fmt.Fprintln(out, "    default:")
+		fmt.Fprintln(out, "        w.WriteHeader(http.StatusNotFound)")
+		fmt.Fprintln(out, "        w.Write([]byte(\"{\\\"error\\\": \\\"unknown method\\\"}\"))")
+		fmt.Fprintln(out, "    }")
+		fmt.Fprintln(out, "}")
 	}
 }
 
-func generateFunc(funcDecl *ast.FuncDecl, out *os.File) {
+func generateStruct(typeSpec *ast.TypeSpec, structType *ast.StructType, out *os.File) {
+	fmt.Fprintln(out, "func (in *"+typeSpec.Name.Name+") ApiGenSet(r *http.Request) error {")
+	for _, field := range structType.Fields.List {
+		if field.Tag == nil {
+			continue
+		}
+
+		tag := reflect.StructTag(field.Tag.Value[1 : len(field.Tag.Value)-1])
+		tagValue := tag.Get("apivalidator")
+		if tagValue == "" {
+			continue
+		}
+
+		fieldName := field.Names[0].Name
+		fieldType := field.Type.(*ast.Ident).Name
+		rules := extractRules(&tagValue)
+		rules.FieldName = fieldName
+		if rules.ParamName == "" {
+			rules.ParamName = strings.ToLower(fieldName)
+		}
+		switch fieldType {
+		case "string":
+			strTpl.Execute(out, rules)
+		case "int":
+			intTpl.Execute(out, rules)
+		default:
+			panic("unsupported")
+		}
+	}
+	fmt.Fprintln(out, "    return nil")
+	fmt.Fprintln(out, "}")
+	fmt.Fprintln(out, "")
+}
+
+func extractRules(tag *string) MethodSetParams {
+	var rules MethodSetParams
+
+	for _, keyValuePair := range strings.Split(*tag, ",") {
+		kV := strings.Split(keyValuePair, "=")
+		if kV[0] == "required" {
+			rules.IsRequired = true
+		}
+
+		if kV[0] == "paramname" {
+			rules.ParamName = kV[1]
+		}
+
+		if kV[0] == "enum" {
+			rules.HasEnum = true
+			rules.EnumString = kV[1]
+		}
+
+		if kV[0] == "default" {
+			rules.HasDefault = true
+			rules.DefaultValue = kV[1]
+		}
+
+		if kV[0] == "min" {
+			rules.HasMin = true
+			rules.Min, _ = strconv.Atoi(kV[1])
+		}
+
+		if kV[0] == "max" {
+			rules.HasMax = true
+			rules.Max, _ = strconv.Atoi(kV[1])
+		}
+	}
+
+	return rules
+}
+
+func generateFunc(funcDecl *ast.FuncDecl, out *os.File, aggr map[string][]ServeHttpParams) {
 	if funcDecl.Doc == nil {
 		return
 	}
@@ -123,10 +354,6 @@ func generateFunc(funcDecl *ast.FuncDecl, out *os.File) {
 	tplParams.ApiGenHttpMethod = signature.Method
 	tplParams.ApiGenParamsType = funcDecl.Type.Params.List[1].Type.(*ast.Ident).Name
 
-	if tplParams.ApiGenMethodName == "" {
-		tplParams.ApiGenMethodName = "GET"
-	}
-
 	if signature.Auth {
 		tplParams.ApiGenAuth = "true"
 	} else {
@@ -134,4 +361,9 @@ func generateFunc(funcDecl *ast.FuncDecl, out *os.File) {
 	}
 
 	wrapperTemplate.Execute(out, tplParams)
+	var serveHttp ServeHttpParams
+	serveHttp.Url = signature.Url
+	serveHttp.Handler = "wrapperApiGen" + tplParams.ApiGenMethodName
+	existed, _ := aggr[tplParams.ApiType]
+	aggr[tplParams.ApiType] = append(existed, serveHttp)
 }
